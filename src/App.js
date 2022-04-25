8af3be702710ca9a6494cefcedc3d229cb6d4dfe
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-24 22:08:11 -0300

    Va queriendo

--- src/App.js
+++ src/App.js
@@ -2,35 +2,47 @@ import './App.css';
 import React from 'react';
 
 function App() {
-  const [input, setInput] = React.useState('0')
+  const [input, setInput] = React.useState('')
+  const [result, setResult] = React.useState('0')
   const [sum, setSum] = React.useState('0')
 
   function handleChange(e) {
-    if (input === '0') {
-      setInput(e.target.value);
+    if (input === '') {
+        setInput(e.target.value);
+        setResult(e.target.value);
+    } else if (result === '+') {
+      setInput(input + e.target.value);
+      setResult(e.target.value);
     } else {
-      setInput(input + e.target.value)
+        setInput(input + e.target.value)
+        setResult(result + e.target.value)
     }
   }
 
   function handleClear() {
-    setInput('0')
+    setInput('')
+    setResult('0')
   }
 
   function handleAdd() {
-    setSum(input)
-  }
+    setInput(input + '+')
+    setResult("+");
+}
 
-  function handleEqual() {
-    
+  function handleEqual(e) {
+    setInput(input + "=" + eval(input));
+    setResult(eval(input))
   }
 
   return (
     <div className='calculatorContainer'>
       <div className='calculator'>
-        <div  className='outputContainer'>
-          <div id='display' className='output'>
+        <div className='outputContainer'>
+          <div id='display' className='input'>
             {input}
+          </div>
+          <div className='result'>
+            {result}
           </div>
         </div>
 
@@ -45,11 +57,11 @@ function App() {
           <button id="four" onClick={handleChange} value={4}>4</button>
           <button id="five" onClick={handleChange} value={5}>5</button>
           <button id="six" onClick={handleChange} value={6}>6</button>
-          <button id="add" onClick={handleChange} value="+">+</button>
+          <button id="add" onClick={handleAdd} value="+">+</button>
           <button id="one" onClick={handleChange} value={1}>1</button>
           <button id="two" onClick={handleChange} value={2}>2</button>
           <button id="three" onClick={handleChange} value={3}>3</button>
-          <button id="equals" onClick={handleChange} value="=">=</button>
+          <button id="equals" onClick={handleEqual} value="=">=</button>
           <button id="zero" onClick={handleChange} value={0}>0</button>
 
           <button id="decimal" onClick={handleChange} value=".">.</button>
