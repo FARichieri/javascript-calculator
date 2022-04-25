4c50727f6a756d6e88603322dca1cd9d01946fec
Fabricio Richieri <fabriciorichieri@yahoo.com>
2022-04-25 01:54:21 -0300

    OK

--- src/App.js
+++ src/App.js
@@ -7,34 +7,40 @@ function App() {
   const [resultStorage, setResultStorage] = React.useState('0')
 
   function handleChange(e) {
-    if (input === '' && e.target.value === '0') {
-      return
-    } else if (result.includes('.') && e.target.value === '.') {
+    if (input.length < 29) {
+      if (input === '' && e.target.value === '0') {
         return
-    } else if (input === '' && e.target.value === '.') {
-        setInput('0.')
-        setResult('0.')
-    } else if (e.target.value === '.' && input.includes("=")) {
-        setInput('0.');
-        setResult('0.');
-    } else if (input === '' || input.includes("=")) {
-        setInput(e.target.value);
-        setResult(e.target.value);
-    } else if (result === '+') {
-        setInput(input + e.target.value);
-        setResult(e.target.value);
-    } else if (result === '-') {
-        setInput(input + e.target.value);
-        setResult(e.target.value);
-    } else if (result === '*') {
-        setInput(input + e.target.value);
-        setResult(e.target.value);
-    } else if (result === '/') {
-        setInput(input + e.target.value);
-        setResult(e.target.value);
+      } else if (result.includes('.') && e.target.value === '.') {
+          return
+      } else if (input === '' && e.target.value === '.') {
+          setInput('0.')
+          setResult('0.')
+      } else if (e.target.value === '.' && input.includes("=")) {
+          setInput('0.');
+          setResult('0.');
+      } else if (input === '' || input.includes("=")) {
+          setInput(e.target.value);
+          setResult(e.target.value);
+      } else if (result === '+') {
+          setInput(input + e.target.value);
+          setResult(e.target.value);
+      } else if (result === '-') {
+          setInput(input + e.target.value);
+          setResult(e.target.value);
+      } else if (result === '*') {
+          setInput(input + e.target.value);
+          setResult(e.target.value);
+      } else if (result === '/') {
+          setInput(input + e.target.value);
+          setResult(e.target.value);
+      } else {
+          setInput(input + e.target.value)
+          setResult(result + e.target.value)
+      }
     } else {
-        setInput(input + e.target.value)
-        setResult(result + e.target.value)
+      let prevVal = result;
+      setResult('Digit Limit Met');
+      setTimeout(() => setResult(prevVal), 1000);
     }
   }
 
@@ -44,62 +50,91 @@ function App() {
   }
 
   function handleAdd() {
+    let last = input[input.length - 1]
+    let last2 = input[input.length - 2]
     if (input.includes('=')) {
       setInput(resultStorage + "+")
       setResult('+')
-    } else if (input[input.length -1] === '-') {
-       setInput(input.replace('-', '+'))
+    } else if (last === '-' && last2 === '*') {
+        setInput(input.replace('*-', '+'))
+        setResult('+')
+    } else if (last === '-') {
+        setInput(input.replace('-', '+'))
+        setResult('+')
+    } else if (last === '*') {
+       setInput(input.replace(last, '+'))
        setResult('+')
-    } else if (input[input.length -1] === '*') {
-       setInput(input.replace('*', '+'))
+    } else if (last === '/') {
+       setInput(input.replace(last, '+'))
        setResult('+')
-    } else if (input[input.length -1] === '/') {
-       setInput(input.replace('/', '+'))
-       setResult('+')
-    } else if (input[input.length -1] !== '+') {
+    } else if (last !== '+') {
         setInput(input + '+')
         setResult("+");
     }
   }
 
   function handleSubtract() {
+    let last = input[input.length -1]
     if (input.includes('=')) {
       setInput(resultStorage + "-")
       setResult('-')
-  
-   } else if (input[input.length -1] === '/') {
-      setInput(input.replace('/', '-'))
-      setResult('-')
-    } else if (input[input.length -1] !== '-') {
+    }  else if (last === '+') {
+        setInput(input.replace('+', '-'))
+        setResult('-')
+    } else if (last !== '-') {
         setInput(input + '-')
         setResult("-");
     }
   }
 
   function handleMultiply() {
+    let last = input[input.length -1]
     if (input.includes('=')) {
       setInput(resultStorage + "*")
       setResult('*')
-    } else if (input[input.length -1] !== '*') {
+    } else if (last === '-') {
+        setInput(input.replace(last, '*'))
+        setResult('*')
+    } else if (last === '+') {
+        setInput(input.replace(last, '*'))
+        setResult('*')
+    } else if (last === '/') {
+        setInput(input.replace(last, '*'))
+        setResult('*')
+    } else if (last !== '*') {
         setInput(input + '*')
         setResult("*");
     }
   }
 
   function handleDivide() {
+    let last = input[input.length -1]
     if (input.includes('=')) {
       setInput(resultStorage + "/")
       setResult('/')
-    } else if (input[input.length -1] !== '/') {
+    } else if (last === '-') {
+      setInput(input.replace(last, '/'))
+      setResult('/')
+    } else if (last === '+') {
+      setInput(input.replace(last, '/'))
+      setResult('/')
+    } else if (last === '*') {
+      setInput(input.replace(last, '/'))
+      setResult('/')
+    } else if (last !== '/') {
         setInput(input + '/')
         setResult("/");
     }
   }
 
   function handleEqual() {
-    setInput(input + "=" + eval(input));
-    setResult(eval(input).toString())
-    setResultStorage(eval(input).toString());
+    if (result !== "0") {
+      setInput(input + "=" + eval(input));
+      setResult(eval(input).toString())
+      setResultStorage(eval(input).toString());
+    } else {
+      setInput('')
+    }
   }
 
   return (
@@ -113,7 +148,6 @@ function App() {
             {result}
           </div>
         </div>
-
         <div className='keys'>
           <button id="clear" onClick={handleClear}>AC</button>
           <button id="divide" onClick={handleDivide} value="/">/</button>
@@ -131,10 +165,9 @@ function App() {
           <button id="three" onClick={handleChange} value={3}>3</button>
           <button id="equals" onClick={handleEqual} value="=">=</button>
           <button id="zero" onClick={handleChange} value={0}>0</button>
-
           <button id="decimal" onClick={handleChange} value=".">.</button>
-
         </div>
+          <p className='by'><a href="https://github.com/FARichieri" target="_blank" rel="noreferrer">By frichieri</a></p>
       </div>  
     </div>
   );
